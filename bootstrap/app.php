d5ea29ca02e631276c476c0954c9f6b5413c201b
Christ Sadjina <sadjinachrist@gmail.com>
2026-07-02 12:01:10 +0000

    Fix HTTPS derriere le proxy Render (trustProxies + APP_URL)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,12 +10,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+        ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+
+        // Faire confiance au proxy HTTPS de Render
+        $middleware->trustProxies(at: '*');
     })
-    
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
